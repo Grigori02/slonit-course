cac89f89497eb30ae1b62d76bd9d8a84d8e90640
Grigori02 <goqayvazyan98@gmail.com>
2025-02-19 20:00:52 +0400

    Created myFilter function

---
+++ myFilter.js
@@ -0,0 +1,21 @@
+const myFilter = (array, callback) => {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i])) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
+
+  const numbers = [1, 2, 3, 4, 5];
+
+  const evenNumbers = myFilter(numbers, (num) => {
+    return num % 2 === 0;
+  });
+  console.log(evenNumbers);
+  
+  const greaterThanThree = myFilter(numbers, (num) => {
+    return num > 3;
+  });
+  console.log(greaterThanThree);
